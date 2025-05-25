6ac10c2baabdd787f7ee1d088a2944341662a069
harshitharayi <harshitha20056@gmail.com>
2025-05-25 20:32:25 +0530

    Added all files required for set up of signup , login , logout and related functionalities

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,35 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
-const app=express();
-const PORT= process.env.PORT;
-app.get("/api/auth/signup", (req,res) => {
-    res.send("Signup Route");
-});
-app.get("/api/auth/login", (req,res) => {
-    res.send("Login Route");
-});
-app.get("/api/auth/logout", (req,res) => {
-    res.send("Logout Route");
-});
-app.listen(PORT , () =>{
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import {connectDB} from "./lib/db.js";
+
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.use("/api/auth" , authRoutes);
+app.use("/api/users" , userRoutes);
+app.use("/api/chat" , chatRoutes);
+
+
+
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+    connectDB();
+
+});
 
